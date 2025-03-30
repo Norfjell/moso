e32499e284babd83cbdbc000822e469480b22699
Liviu <bibire.liviu@gmail.com>
2025-03-30 10:04:43 +0300

    Initial commit

---
+++ packages/Moso/ns-theme-agency/ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ns_theme_agency".
+ *
+ * Auto generated 11-05-2023 13:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF['ns_theme_agency'] = array (
+  'title' => 'T3 Agency TYPO3 Template',
+  'description' => 'T3 Agency Template for TYPO3 Website, for Build your TYPO3 website for a software development company, a product startup or any new business. plus its compatible with the latest TYPO3 v12 & v13.',
+  'category' => 'templates',
+  'version' => '13.0.5',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'author' => 'Team NITSAN',
+  'author_email' => 'info@nitsan.in',
+  'author_company' => 'NITSAN Technologies Pvt Ltd',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '12.0.0-13.4.99',
+      'ns_basetheme' => '13.0.0-13.5.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
